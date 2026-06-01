65378df94171342feda6ff8ef46e9aaae9262398
Srbaqua <23bcs101@nith.ac.in>
2026-06-01 16:27:03 +0530

    backend modified to resolve error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import {Server} from "socket.io";
 import http from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 
 const app = express();
@@ -9,29 +9,37 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"*",        
-    },
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on("connection",(socket)=>{
-    console.log("User Connected:",socket.id);
-    socket.on("Join-room",(roomId:string)=>{
-        socket.join(roomId);
-        console.log(`${socket.id} joined ${roomId}`);
-        socket.to(roomId).emit("user-join",socket.id);
-    });
-    socket.on("send-message",({roomId,message})=>{
-        socket.to(roomId).emit("recieve-message",message);
-    });
-    socket.on("disconnect",()=>{
-        console.log("Disconnected:",socket.id);
-    });
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("join-room", (roomId: string) => {
+    console.log("JOIN ROOM EVENT RECEIVED:", roomId);
+
+    socket.join(roomId);
+
+    console.log(`${socket.id} joined ${roomId}`);
+
+    socket.to(roomId).emit("user-joined", socket.id);
+  });
+
+  socket.on("send-message", ({ roomId, message }) => {
+    console.log("MESSAGE EVENT:", roomId, message);
+
+    socket.to(roomId).emit("receive-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
 });
+
 const PORT   = 5000
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
-
-
